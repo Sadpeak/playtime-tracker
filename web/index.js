a2c432f7aac3eaa3355f64833db7fec6ffc02f97
Sadpeak <maxstack@mail.ru>
2021-07-25 23:58:48 +0300

    rewrite web panel to nodejs

---
+++ web/index.js
@@ -0,0 +1,64 @@
+const mysql = require('mysql');
+const express = require('express')
+require('dotenv').config()
+
+const { toJson, timeConverter, flagsString, toSteamID64} = require('./utils')
+
+const con = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
+});
+
+
+// const querryTotal = `SELECT steamid, name, GROUP_CONCAT(DISTINCT name SEPARATOR '\n') AS names, SUM(end - start) AS total, COUNT(*) AS sessions, GROUP_CONCAT(DISTINCT flags SEPARATOR '\n') AS flags FROM \`playtime_tracker\`  GROUP BY steamid ORDER BY total DESC LIMIT 5`
+
+
+
+const app = express()
+const port = 80
+
+app.set('view engine', 'ejs')
+
+app.get('/search', (req, res) => {
+
+   if(!req.query.steamid)
+   res.render('search')
+
+   if(req.query.steamid){
+   const querryT = `SELECT steamid, name, GROUP_CONCAT(DISTINCT name SEPARATOR '\n') AS names, SUM(end - start) AS total, COUNT(*) AS sessions, GROUP_CONCAT(DISTINCT flags SEPARATOR '|') AS flags FROM \`playtime_tracker\` WHERE steamid = "${req.query.steamid}"`
+   const querryD = `SELECT SUM(end - start) AS totalDay FROM \`playtime_tracker\` WHERE start > UNIX_TIMESTAMP(DATE_SUB(CURRENT_TIMESTAMP , INTERVAL 1 DAY)) AND steamid = "${req.query.steamid}"`
+   const querryW = `SELECT SUM(end - start) AS totalWeek FROM \`playtime_tracker\` WHERE start > UNIX_TIMESTAMP(DATE_SUB(CURRENT_TIMESTAMP , INTERVAL 1 WEEK)) AND steamid = "${req.query.steamid}"`
+   const querryM = `SELECT SUM(end - start) AS totalMonth FROM \`playtime_tracker\` WHERE start > UNIX_TIMESTAMP(DATE_SUB(CURRENT_TIMESTAMP , INTERVAL 1 MONTH)) AND steamid = "${req.query.steamid}"`
+   
+   let stats = {}
+
+   con.query(querryW, function (err, result, fields) { stats.totalWeek = timeConverter(toJson(result)[0].totalWeek) })
+   con.query(querryM, function (err, result, fields) { stats.totalMonth = timeConverter(toJson(result)[0].totalMonth) })
+   con.query(querryD, function (err, result, fields) { stats.totalDay = timeConverter(toJson(result)[0].totalDay) })
+   
+   con.query(querryT, function (err, result, fields) {
+      result = toJson(result)
+      stats.total = timeConverter(result[0].total)
+      stats.flags = flagsString(result[0].flags.split('|').pop())
+      stats.steamLink = toSteamID64(result[0].steamid)
+      stats.sessions = result[0].sessions
+      stats.name = result[0].name
+      stats.names = result[0].names
+      stats.steamid = result[0].steamid
+
+      console.log(stats)
+      res.render('searchRes', {res: stats})
+          
+   
+   });
+}
+
+})
+
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
+
---
+++ web/utils.js
@@ -0,0 +1,31 @@
+const toJson = data => JSON.parse(JSON.stringify(data))
+
+
+const timeConverter = (time) => {
+   timeH = Math.trunc(time/3600)
+   timeM = Math.trunc((time - timeH*3600)/60)
+   timeS = time - timeH*3600 - timeM*60
+
+   return timeH +':'+ timeM +':'+ timeS
+}
+
+
+const flagsString = flags => {
+   
+    const alf = 'abcdefghijklmnzopqrst'
+    let s = ''
+    for (let i = 0; i < alf.length; i++) {
+       if (flags & (2 ** i)){
+          s += alf[i]
+         }       
+    }
+    return s
+}
+
+const toSteamID64 = (steamid)=> {
+        const splited = steamid.split(':')
+        return 'https://steamcommunity.com/profiles/' + String(BigInt(splited[2]*2)+76561197960265728n + BigInt(splited[1]))
+      }
+
+
+module.exports = { toJson, timeConverter, flagsString, toSteamID64}
